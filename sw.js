e41faf5d3967f55629dd3d03f3dd85d1713a7407
Delts <63209577+Delta-1@users.noreply.github.com>
2026-06-12 08:46:07 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'biblionmed-v1';
+
+// Ativa o Service Worker imediatamente
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    return self.clients.claim();
+});
+
+// Estratégia simples de rede para não quebrar a sincronização do seu CSV
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            return caches.match(event.request);
+        })
+    );
+});
